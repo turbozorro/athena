7daeff2ca70c254fd43fe91c220639138b2534da
JuanPa Medina Bernal <software.jpmb@gmail.com>
2024-04-27 19:50:39 -0500

    Added models and connected to templates

--- athena/admin.py
+++ athena/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class UserTaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'user', 'completion', 'grade')
+
+class UserSubjectAdmin(admin.ModelAdmin):  
+    list_display = ('subject', 'user', 'completion', 'grade')
+
+class TaskAdmin(admin.ModelAdmin):  
+    list_display = ('name', 'subject')
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Subject)
+admin.site.register(UserSubject, UserSubjectAdmin)
+admin.site.register(UserTask, UserTaskAdmin)
